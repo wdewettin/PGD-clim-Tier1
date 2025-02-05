974c326434feec1254e789b6665b476e0c0dde92
wdwettin <wout.dewettinck@ugent.be>
2025-02-05 15:02:06 +0100

    Further cleanup of files

---
+++ climake/hack/overwriteEcoclimap.sh
@@ -0,0 +1,4 @@
+#>>> overwrite ECOCLIMAP I files by ECOCLIMAP II
+chmod 644 ecoclimap.dir ecoclimap.hdr
+cp /dodrio/scratch/users/vsc45263/wout/PGD-clim-Tier1/climake/data/ECOCLIMAP_II_EUROP_V2.5.dir ecoclimap.dir
+cp /dodrio/scratch/users/vsc45263/wout/PGD-clim-Tier1/climake/data/ECOCLIMAP_II_EUROP_V2.5.hdr ecoclimap.hdr
