63e47528955c70295d3aaff1b1bbb89990e09898
amberJ99 <amber.99.jacobs@gmail.com>
2026-01-12 13:30:38 +0100

    Fix range legend (to exclude NaN)

--- climake/check_output/plot_all_fields_PGD.R
+++ climake/check_output/plot_all_fields_PGD.R
@@ -26,11 +26,21 @@ print("CHECK1")
 
 pdf(pdffilename, width = 8, height = 7)
 
+fields_with_nan <- list( "SFX.SSO_STDEV   ", "SFX.SSO_ANIS    ","SFX.SSO_DIR     ","SFX.SSO_SLOPE   ","SFX.HO2IP       ","SFX.HO2JP       ","SFX.HO2IM       ","SFX.HO2JM       ","SFX.AOSIP       ","SFX.AOSJP       ","SFX.AOSIM       ","SFX.AOSJM       ","SFX.BATHY       ","SFX.CLAY        ","SFX.SAND        ","SFX.RUNOFFB     ","SFX.WDRAIN      ","SFX.GD_CLAY     ","SFX.GD_SAND     ","SFX.GD_RUNOFFB  ","SFX.GD_WDRAIN   ")
+print(fields_with_nan)
+
 for (idx in seq_along(varname_list["name"][[1]])) {
     varname <- varname_list["name"][[1]][[idx]]
     y <- try(FAdec(x, varname), silent=TRUE)
     if (!inherits(y, "try-error")) {
-        iview(y, legend=TRUE)
+      if (varname %in% fields_with_nan){
+        print("Found field with NaN")
+        print(varname)
+        y_nonan <- y
+        y_nonan[y_nonan>=1e20] <- NA
+        iview(y_nonan, legend=TRUE)} 
+        else {
+        iview(y, legend=TRUE)}
     }
 }
 
